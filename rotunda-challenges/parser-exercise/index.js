c84d6eb51215ef14e8d901279abe161c1cd18629
Sabrina Sproviero <sabrinakay@Sabrinas-MacBook-Pro.local>
2023-09-12 16:11:00 -0300

    init rotunda challenges

---
+++ rotunda-challenges/parser-exercise/index.js
@@ -0,0 +1,27 @@
+"use strict";
+const urlParser = (formatString, urlInstance) => {
+    //splits the URL format string
+    const formatParts = formatString.split('/');
+    //splits the URL instance into
+    const urlParts = urlInstance.split('/');
+    const queryParams = new URLSearchParams(urlInstance.split('?')[1] || '');
+    const result = {};
+    for (let i = 0; i < formatParts.length; i++) {
+        const formatPart = formatParts[i];
+        const urlPart = urlParts[i];
+        //iterates to extract the variable parts (those starting with ':') 
+        if (formatPart.startsWith(':')) {
+            const key = formatPart.substring(1);
+            result[key] = urlPart;
+        }
+    }
+    //creates hash key:value
+    queryParams.forEach((value, key) => {
+        result[key] = isNaN(parseInt(value)) ? value : parseInt(value);
+    });
+    return result;
+};
+const formatString = '/:version/api/:collection/:id';
+const urlInstance = '/6/api/listings/3?sort=desc&limit=10';
+const parsedData = urlParser(formatString, urlInstance);
+console.log(parsedData);
